31cbbb152380a3c870bf7b8e0f7903bdb85f2e57
AgenciaSeniors <seniors.contacto@gmail.com>
2025-12-15 18:37:21 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://qspwtmfmolvqlzsbwlzv.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_ba5r8nJ5o49w1b9TURDLBA_EbMC_lWU',
+    // Usamos el modelo más estable para evitar errores 404
+    GEMINI_KEY: 'AIzaSyAQNSLLZSNIWjAW4b9Ifke1TWT5YPtZ0cE' 
+};
+
+// Cliente Global de Supabase
+const supabaseClient = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
