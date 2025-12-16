e5c99f377df62bfd496cfffa927a8ebaf598356a
AgenciaSeniors <seniors.contacto@gmail.com>
2025-12-16 00:55:03 -0600

    ok

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const CONFIG = {
     SUPABASE_URL: 'https://mvtatdvpsjynvayhhksc.supabase.co',
     SUPABASE_KEY: 'sb_publishable_XtV2kYHISXME2K-STuHmdw_UUGTZyvS',
-    // Usamos el modelo más estable para evitar errores 404
-    GEMINI_KEY: 'AIzaSyA5Ku1Rk1dXTl_pfXQ3wEwvkaENfguMYN8' 
 };
 
 // Cliente Global de Supabase
