1a68e967dca552550646054fb36cb2259d9fd9e2
AgenciaSeniors <seniors.contacto@gmail.com>
2025-12-16 01:13:09 +0100

    Update GEMINI_KEY in config.js

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ const CONFIG = {
     SUPABASE_URL: 'https://mvtatdvpsjynvayhhksc.supabase.co',
     SUPABASE_KEY: 'sb_publishable_XtV2kYHISXME2K-STuHmdw_UUGTZyvS',
     // Usamos el modelo más estable para evitar errores 404
-    GEMINI_KEY: 'AIzaSyAQNSLLZSNIWjAW4b9Ifke1TWT5YPtZ0cE' 
+    GEMINI_KEY: 'AIzaSyDDqPRUFilvcpVlFlMnxkYYju4RnVAFGv4' 
 };
 
 // Cliente Global de Supabase
+
 const supabaseClient = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
